08afe7410a4c17c192d75e10c3ee066306ccd3ba
AnastasiaGandolfi <118977246+AnastasiaGandolfi@users.noreply.github.com>
2023-02-27 21:08:04 +0100

    exercise done

--- my-app/src/Welcome.js
+++ my-app/src/Welcome.js
@@ -4,11 +4,12 @@ export class Welcome extends React.Component {
     render() {
         return (
             <div>
-            {this.props.name 
+                {this.props.name 
                     ? <h1>Welcome, {this.props.name}!</h1>
                     : <h1>Welcome, Anastasia!</h1>
 
-            }
+                }
+                <p>Your age is {this.props.age}</p>
             </div>
         )
     }
