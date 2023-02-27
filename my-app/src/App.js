58de9a5ec0525b319ab2ed92966830bb8eb08190
AnastasiaGandolfi <118977246+AnastasiaGandolfi@users.noreply.github.com>
2023-02-27 17:32:13 +0100

    exercise done

---
+++ my-app/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Hello } from "./HelloWorld";
+
+export class App extends React.Component {
+    render() {
+        return(
+            <div><Hello /></div>
+        )
+    }
+}
