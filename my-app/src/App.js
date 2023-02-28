b9701af0a28c411ea467e5c930c2e80f1338a94b
AnastasiaGandolfi <118977246+AnastasiaGandolfi@users.noreply.github.com>
2023-02-28 12:01:19 +0100

    exercise done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ export class App extends React.Component {
         return(
             <div>
                 <Hello />
-                <Welcome name={<strong>Anastasia</strong>} />
+                <Welcome age={27} name={<strong>Anastasia</strong>} />
             </div>
         )
     }
--- my-app/src/Welcome.js
+++ my-app/src/Welcome.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { Age } from "./Age";
 
 class Welcome extends React.Component {
     render() {
         return (
             <div>
                 <h1>Welcome, {this.props.name}!</h1>
+                <Age age={this.props.age} />
             </div>
         )
     }
