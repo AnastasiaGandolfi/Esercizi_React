dcb03143538f7441506c06a817711a32bb3db492
AnastasiaGandolfi <118977246+AnastasiaGandolfi@users.noreply.github.com>
2023-03-01 16:14:08 +0100

    exercise done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+function Sum(a, b) {
+  let result = a + b;
+  return result
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h2>{Sum(5, 5)}</h2>
   );
 }
 
-export default App;
+export default App
