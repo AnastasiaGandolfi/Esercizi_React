f3532a2837c118726f4d4adc17ef57548140273b
AnastasiaGandolfi <118977246+AnastasiaGandolfi@users.noreply.github.com>
2023-03-02 10:41:27 +0100

    exercise done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ export class App extends React.Component {
         return(
             <div>
                 <Hello />
-                <Welcome age={67} />
+                <Welcome age={64} name={"John"}/>
             </div>
         )
     }
