4fada8a06bd5c6729fd25cf4339a41d4b6942708
AnastasiaGandolfi <118977246+AnastasiaGandolfi@users.noreply.github.com>
2023-03-04 18:27:13 +0100

    exercise done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { ClickTracker } from "./ClickTracker";
+import { ClickCounter } from "./ClickCounter";
 
 export class App extends React.Component {
     render() {
         return(
             <div>
-                <ClickTracker />
+                <ClickCounter />
             </div>
         )
     }
