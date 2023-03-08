01c37f29ffc6ed62cd33b901a1143ad2044a2dd0
AnastasiaGandolfi <118977246+AnastasiaGandolfi@users.noreply.github.com>
2023-03-08 13:37:23 +0100

    exercise done

--- my-app/src/TodoList.js
+++ my-app/src/TodoList.js
@@ -1,4 +1,4 @@
-// Create a TodoList component that renders a ul tag with a li tag for each item contained in the items state variable. The items state variable should be an array of strings. The TodoList component should also contain an input tag and a button. When the button is clicked, the event handler should add the value of the input tag to the items array.
+// Modify the TodoList component so that the input clears every time a Todo is added to the items array.
 
 import React, { Component } from 'react'
 
@@ -23,7 +23,7 @@ export default class TodoList extends Component {
                         {this.state.items.map(item => <li>{item}</li>)}
                     </ul>
                     <input type="text" id="input"></input>
-                    <button type='submit' >Add item</button>
+                    <button type='submit'>Add item</button>
                 </form>
             </div>
         )
