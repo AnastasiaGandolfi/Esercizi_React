3318c56c07a9123c9d5c60ac51d12c484c7d26f6
AnastasiaGandolfi <118977246+AnastasiaGandolfi@users.noreply.github.com>
2023-03-08 13:47:43 +0100

    exercise done

--- my-app/src/TodoList.js
+++ my-app/src/TodoList.js
@@ -1,4 +1,4 @@
-// Create a TodoList component that renders a ul tag with a li tag for each item contained in the items state variable. The items state variable should be an array of strings. The TodoList component should also contain an input tag and a button. When the button is clicked, the event handler should add the value of the input tag to the items array.
+// Modify the TodoList by adding a "reset" button that clears the items array when clicked.
 
 import React, { Component } from 'react'
 
@@ -6,13 +6,20 @@ export default class TodoList extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            items: ["preparare il pranzo", "allenarsi", "studiare", "fare gli esercizi"]
+            items: []
         }
     }
     handleAddItem = (event) => {
         event.preventDefault()
         let newValue = document.getElementById('input').value
         this.setState((prevState) => ({ items: [...prevState.items, newValue] }))
+        document.getElementById('input').value = '';
+    }
+
+    handleReset = () => {
+        this.setState({
+            items:[]
+        })
     }
     render() {
         return (
@@ -22,7 +29,8 @@ export default class TodoList extends Component {
                         {this.state.items.map(item => <li>{item}</li>)}
                     </ul>
                     <input type="text" id="input"></input>
-                    <button type='submit' >Add item</button>
+                    <button type='submit'>Add item</button>
+                    <button type='reset' onClick={this.handleReset}>Reset Todo List</button>
                 </form>
             </div>
         )
