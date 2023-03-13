3d4432956a0c9588e822e8c390fb2a33ee46c1ff
AnastasiaGandolfi <118977246+AnastasiaGandolfi@users.noreply.github.com>
2023-03-13 13:45:26 +0100

    minor changes

--- my-app/src/TodoList.js
+++ my-app/src/TodoList.js
@@ -1,42 +1,26 @@
-// Modify the TodoList component so that, instead of rendering the items array within the ul tag, it calls the function passed to its render prop, passing it the items array as a parameter, as well as the function required to delete the item. Pass a render prop to the TodoList component to correctly render and interact with the items array.
-
-import React, { Component, createRef } from 'react'
+import React, { Component } from 'react'
 
 export default class TodoList extends Component {
-    formRef = createRef()
-
     constructor(props) {
         super(props);
         this.state = {
-            items: []
+            items: ["preparare il pranzo", "allenarsi", "studiare", "fare gli esercizi"]
         }
     }
-
     handleAddItem = (event) => {
         event.preventDefault()
-        let { todo } = this.formRef.current.elements
-        this.setState((prevState) => ({ items: [...prevState.items, todo.value] }))
+        let newValue = document.getElementById('input').value
+        this.setState((prevState) => ({ items: [...prevState.items, newValue] }))
     }
-
-    handleRemoveItem = (value) => {
-        this.setState((prevState) => ({
-            items: prevState.items.filter((item, index) => index !== value)
-        }))
-    }
-
-    handleReset = () => {
-        this.setState({
-            items: []
-        })
-    }
-
     render() {
         return (
             <div>
-                <form ref={this.formRef} onSubmit={this.handleAddItem}>
-                    {this.props.render(this.state.items, this.handleRemoveItem)}
-                    <input  type="text" name="todo" className='border border-blue-400'></input>
-                    <button type='submit' className='border border-blue-400'>Add item</button>
+                <form onSubmit={this.handleAddItem}>
+                    <ul>
+                        {this.state.items.map(item => <li>{item}</li>)}
+                    </ul>
+                    <input type="text" id="input"></input>
+                    <button type='submit' >Add item</button>
                 </form>
             </div>
         )
