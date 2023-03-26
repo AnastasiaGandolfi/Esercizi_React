f5d28941271d1ce60d08516a8c258c4fe03a3577
AnastasiaGandolfi <118977246+AnastasiaGandolfi@users.noreply.github.com>
2023-03-26 17:24:01 +0200

    exercise done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,43 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import { Counter } from "./Counter";
 import ShowGithubUser from "./ShowGithubUser";
 import Welcome from "./Welcome";
 
-// Add a new Route to the users/:username path that renders a ShowGithubUser component that receives the username as a path parameter and renders the GithubUser component from useEffect 03 by passing it the received username.
+// Add three Links within the main App component and use them to navigate to all three routes.
 
 export class App extends React.Component {
     render() {
         return (
             <div>
                 <Routes>
-                    <Route path='/'element={<div><Welcome name={'Anastasia'} /></div>} />
-                    <Route path="/counter" element={<Counter />}/>
-                    <Route path="users/:username" element={<ShowGithubUser />}/>
+                    <Route path='/' element={
+                        <div>
+                            <Welcome name={'Anastasia'} />
+                            <div><Link to='/counter'>
+                                Go to the counter
+                            </Link>
+                            </div>
+                            <div><Link to='users/AnastasiaGandolfi'>
+                                Go to the Github User
+                            </Link>
+                            </div>
+                        </div>} />
+                    <Route path="/counter" element={
+                        <div>
+                            <Counter />
+                            <Link to='/'>
+                                Go home
+                            </Link>
+                        </div>} />
+                    <Route path="users/:username" element={
+                        <div>
+                            <ShowGithubUser />
+                            <Link to='/'>
+                                Go home
+                            </Link>
+                        </div>
+                    } />
                 </Routes>
             </div >
         )
