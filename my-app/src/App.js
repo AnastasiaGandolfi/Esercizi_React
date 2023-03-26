0eb53629ec15d1a6f40f70a67ea111cb23aa9d6b
AnastasiaGandolfi <118977246+AnastasiaGandolfi@users.noreply.github.com>
2023-03-26 17:12:50 +0200

    exercise done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,19 @@
 import React from "react";
-import { Link, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Counter } from "./Counter";
+import ShowGithubUser from "./ShowGithubUser";
 import Welcome from "./Welcome";
 
-// Add a new Route to the /counter path that renders the Counter component from useState 01.
+// Add a new Route to the users/:username path that renders a ShowGithubUser component that receives the username as a path parameter and renders the GithubUser component from useEffect 03 by passing it the received username.
 
 export class App extends React.Component {
     render() {
         return (
             <div>
                 <Routes>
-                    <Route path='/'
-                        element={
-                            <div>
-                                <Welcome name={'Anastasia'} />
-                            </div>
-                        } />
-
-                    <Route path="/counter" element={<Counter />}>
-                    </Route>
+                    <Route path='/'element={<div><Welcome name={'Anastasia'} /></div>} />
+                    <Route path="/counter" element={<Counter />}/>
+                    <Route path="users/:username" element={<ShowGithubUser />}/>
                 </Routes>
             </div >
         )
